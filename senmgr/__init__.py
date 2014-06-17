e8404cc011ee1c8e4bdfb68db24012cd4e00a82c
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-16 23:30:14 -0400

    handling configuration, and db connection

--- senmgr/__init__.py
+++ senmgr/__init__.py
@@ -2,12 +2,16 @@ from gevent import monkey
 monkey.patch_all()
 from flask import Flask
 from flask.ext.socketio import SocketIO
+from flask.ext.sqlalchemy import SQLAlchemy
 import redis
+from config import Default
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config.from_object(Default)
 con = redis.StrictRedis()
 
 ws = SocketIO(app)
+
+db = SQLAlchemy()
 
 from . import views
