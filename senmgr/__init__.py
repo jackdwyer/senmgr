ef3b2e8c0420f12dfa49eacd7c5bad59e8acdf10
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-17 23:49:38 -0400

    moved sensors import to after db creation, and gave db instance of app

--- senmgr/__init__.py
+++ senmgr/__init__.py
@@ -5,16 +5,16 @@ from flask.ext.socketio import SocketIO
 from flask.ext.sqlalchemy import SQLAlchemy
 import redis
 from config import Default
-from sensors.views import sensors
 
 app = Flask(__name__)
 app.config.from_object(Default)
 con = redis.StrictRedis()
-app.register_blueprint(sensors, url_prefix='/sensor')
-
 ws = SocketIO(app)
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 from . import views
-from sensors import models
+
+from sensors.models import Sensor
+from sensors.views import sensors
+app.register_blueprint(sensors, url_prefix='/sensor')
