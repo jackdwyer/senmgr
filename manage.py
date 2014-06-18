c7470f8ebc26105c6f738a035a2c92dd3f5a0f92
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-17 22:00:32 -0400

    added argument to create initial database

--- manage.py
+++ manage.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python2
-from senmgr import app, ws
+import argparse
+from senmgr import app, ws, db
+
+parser = argparse.ArgumentParser(description="Sensor Management")
+parser.add_argument("-create_db", action='store_true')
+args = parser.parse_args()
+
+if args.create_db:
+    def create_db():
+        db.create_all()
 
 if __name__ == "__main__":
     ws.run(app)
