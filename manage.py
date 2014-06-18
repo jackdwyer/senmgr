2c4a641d524dd39f149c204fe2efeafcb4eca19e
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-17 23:31:42 -0400

    removed unnecessary function, added -world arg, for some testing

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python2
 import argparse
-from senmgr import app, ws, db
+from senmgr import app, ws, db, config
 
 parser = argparse.ArgumentParser(description="Sensor Management")
 parser.add_argument("-create_db", action='store_true')
+parser.add_argument("-world", action='store_true')
 args = parser.parse_args()
 
 if args.create_db:
-    def create_db():
-        db.create_all()
+    db.create_all()
 
 if __name__ == "__main__":
-    ws.run(app)
+    if args.world:
+        ws.run(app, host="0.0.0.0")
+    else:
+        ws.run(app)
