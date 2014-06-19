a85b5827f7d82a9e8067c0b2a7f507076e81dc35
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-18 23:49:17 -0400

    using wtforms, for validation of sensor registration

--- senmgr/sensors/views.py
+++ senmgr/sensors/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
-from .models import Sensor
+from flask import Blueprint, jsonify, request
+from models import Sensor
+from forms import SensorForm
 from . import db
 
 sensors = Blueprint('sensors', __name__,
@@ -9,14 +10,18 @@ sensors = Blueprint('sensors', __name__,
 def index():
     return "sensor blueprint"
 
-#basic register function
-@sensors.route('/register/<sensor_name>')
-def register(sensor_name):
-    sensor = Sensor(sensor_name, "description place holder")
-    db.session.add(sensor)
-    db.session.commit()
-    return "Sensor: {0} added".format(sensor_name) 
-
 @sensors.route('/all')
 def get_all_sensors():
     return jsonify(sensors=[val.serialise for val in Sensor.query.all()])
+
+@sensors.route('/register/<sensor_name>', methods=['POST'])
+def register(sensor_name):
+    form = SensorForm(request.form)
+    if form.validate():
+        return "Sensor: {0} added".format(sensor_name)
+    #sensor = Sensor(sensor_name, "description place holder")
+    #db.session.add(sensor)
+    #db.session.commit()
+    return "nope" 
+
+
