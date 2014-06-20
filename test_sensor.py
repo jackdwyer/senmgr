37c8092053bf2cac04d611b78777b1e4fca30363
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-19 21:55:23 -0400

    basic http request for registration

--- test_sensor.py
+++ test_sensor.py
@@ -1,8 +1,13 @@
-import redis
-import time
-r = redis.Redis()
-i = 0
-while True:
-    r.publish('test', str(i))
-    i += 1
-    time.sleep(0.1)
+import requests
+data = {"sensor_key":"test_senagaagaga"}
+
+r = requests.post("http://localhost:5000/sensor/register", data=data)
+print r.text
+#import redis
+#import time
+#r = redis.Redis()
+#i = 0
+#while True:
+#    r.publish('test', str(i))
+#    i += 1
+#    time.sleep(0.1)
