bd4e0f9c8115a8c475d8cd8f3995ee165e20ce1b
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-22 23:49:54 -0400

    configuration for unittests

--- senmgr/config.py
+++ senmgr/config.py
@@ -2,3 +2,9 @@ class Default(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/senmgr.db'
     SECRET_KEY = 'dev'
+
+class Test(object):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/senmgr-unittests.db'
+    SECRET_KEY = 'test'
+    
