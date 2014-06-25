e56ccd4261c0f96b95385249c0f44ca8b4ff1fb4
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-24 22:09:03 -0400

    using json decorator

--- senmgr/sensors/views.py
+++ senmgr/sensors/views.py
@@ -2,29 +2,35 @@ from flask import Blueprint, jsonify, request, render_template
 from sqlalchemy.orm.exc import NoResultFound
 from models import Sensor
 from forms import SensorForm
+from .utils import json
 from . import db
 
 sensors = Blueprint('sensors', __name__,
     template_folder='templates')
 
+
 @sensors.route('/')
 def index():
     return "sensor blueprint"
 
+
 @sensors.route('/all')
+@json
 def get_all_sensors():
-    return jsonify(sensors=[val.serialise for val in Sensor.query.all()])
+    return {'sensors': [val.serialise for val in Sensor.query.all()]}
+
 
 @sensors.route('/register', methods=['POST'])
+@json
 def register():
     form = SensorForm(request.form)
     if form.validate():
         try:
             sensor = Sensor.query.filter(Sensor.sensor_key == form.data.get('sensor_key')).one()
-            return jsonify({"sensor_id":str(sensor.id)}), 202
+            return {"sensor_id": str(sensor.id)}, 202
         except NoResultFound:
             sensor = Sensor(form.data.get('sensor_key'), form.data.get('description'))
             db.session.add(sensor)
             db.session.commit()
-            return jsonify({"sensor_id":str(sensor.id)}), 201
-    return "invalid", 400
+            return {"sensor_id": str(sensor.id)}, 201
+    return {"error": "invalid"}, 400
