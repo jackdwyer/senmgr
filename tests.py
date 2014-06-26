0ef76d32f93062bc5701f41596f00bcea3cb7165
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-25 21:50:22 -0400

    cleaning up formatting

--- tests.py
+++ tests.py
@@ -8,21 +8,22 @@ import os
 from senmgr import app, db, ws
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/unittest-db.db"
-app.config["DEBUG"] = False 
+app.config["DEBUG"] = False
 db.create_all()
 
 Thread(target=ws.run, args=[app]).start()
 
+
 class CreateSensor(unittest.TestCase):
     def test_create_sensor(self):
-        data = {"sensor_key":"test_sensor_unittest"}
+        data = {"sensor_key": "test_sensor_unittest"}
         r = requests.post("http://localhost:5000/sensor/register", data=data)
-        self.assertEqual(r.status_code, 201)    
+        self.assertEqual(r.status_code, 201)
 
     def test_duplicate_sensor(self):
-        data = {"sensor_key":"test_sensor_unittest"}
+        data = {"sensor_key": "test_sensor_unittest"}
         r = requests.post("http://localhost:5000/sensor/register", data=data)
-        self.assertEqual(r.status_code, 202)    
+        self.assertEqual(r.status_code, 202)
         self.assertIsInstance(r.json(), dict)
 
     def test_kill_server(self):
