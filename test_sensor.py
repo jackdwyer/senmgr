7141af9a952e5116583838fbe7771a77cf4fee32
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-29 17:18:17 -0400

    some fake sensor data

--- test_sensor.py
+++ test_sensor.py
@@ -3,15 +3,23 @@ data = {"sensor_key":"test_senagaagaga"}
 
 r = requests.post("http://localhost:5000/sensor/register", data=data)
 print r.text
-#import redis
-#import time
-#r = redis.Redis()
-#i = 0
-#while True:
-#    r.publish('test', str(i))
-#    i += 1
-#    time.sleep(0.1)
-data = {"sensor_key":"test_ssensor", "description":"hey im a sensor"}
-r = requests.post("http://localhost:5000/sensor/register", data=data)
+import redis
+import time
+sen1 = {"sensor_key":"test_ssensor", "description":"hey im a sensor"}
+sen2 = {"sensor_key":"test_2", "description":"hey im a sensor"}
+requests.post("http://localhost:5000/sensor/register", data=sen1)
+requests.post("http://localhost:5000/sensor/register", data=sen2)
+
+r = redis.Redis()
+i = 0
+j = 10000000000000
+while True:
+    r.publish(sen1["sensor_key"], str(i))
+    r.publish(sen2["sensor_key"], str(j))
+    i += 1
+    j -= 1
+    time.sleep(0.1)
+
+
 print r.text
 
