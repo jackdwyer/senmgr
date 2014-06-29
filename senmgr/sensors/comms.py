a8973869ece0c8a04f75ee5d4dd3261bdd81ef75
Jack Dwyer <jackjack.dwyer@gmail.com>
2014-06-29 17:17:35 -0400

    passing sensor key

--- senmgr/sensors/comms.py
+++ senmgr/sensors/comms.py
@@ -1,11 +1,14 @@
+from models import Sensor
+
+
 def get_sensor_list(redis):
-    #TODO return list of sensors that have been registered
-    sensor_list = ['test']
-    return sensor_list
+    return [val.sensor_key for val in Sensor.query.all()]
+
 
 def subscribe(redis, sensor_list, ws):
     r = redis.pubsub()
     r.subscribe(sensor_list)
     for item in r.listen():
+        print item
         ws.emit('sensor_data',
-                        {'data': item['data'], 'type':'sensor'})
+                        {'data': item['data'], 'type':item['channel']})
